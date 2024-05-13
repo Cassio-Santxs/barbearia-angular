e1288115c3fc976a045cb48f60836bfbc6368387
Japorona <130513949+Japorona@users.noreply.github.com>
2024-05-13 19:57:05 -0300

    tela login pronta

--- src/app/components/layout/login/login.component.ts
+++ src/app/components/layout/login/login.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MdbFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
-export class LoginComponent {
 
+export class LoginComponent {
+  dsLogin!: string;
+  dsSenha!: string;
+
+  router = inject(Router);
+
+  logar() {
+    if (this.dsLogin == 'admin' && this.dsSenha == '123') 
+      this.router.navigate(['admin/']);
+    else
+      alert('Login ou senha incorretos');
+  }
 }
