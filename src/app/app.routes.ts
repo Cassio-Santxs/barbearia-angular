d7492e313e986f0b86480a746af110dacf6305f5
Cássio Vagner dos Santos Diogo <cassiovsd@hotmail.com>
2024-05-21 20:58:16 -0300

    correção

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/layout/login/login.component';
 import { PrincipalComponent } from './components/layout/principal/principal.component';
-import { LivroslistComponent } from './components/livro/livroslist/livroslist.component';
-import { LivrosdetailsComponent } from './components/livro/livrosdetails/livrosdetails.component';
 import { HorariolistComponent } from './components/horario/horariolist/horariolist.component';
 import { HorariodetailsComponent } from './components/horario/horariodetails/horariodetails.component';
 
@@ -10,9 +8,6 @@ export const routes: Routes = [
     { path: "", redirectTo: "login", pathMatch: 'full' },
     { path: "login", component: LoginComponent },
     { path: "admin", component: PrincipalComponent, children: [
-        {path: "livros", component: LivroslistComponent},
-        {path: "livros/new", component: LivrosdetailsComponent},
-        {path: "livros/edit/:id", component: LivrosdetailsComponent},
         {path: "horarios", component: HorariolistComponent},
         {path: "horarios/new", component: HorariodetailsComponent},
         {path: "horarios/edit/:id", component: HorariodetailsComponent},
