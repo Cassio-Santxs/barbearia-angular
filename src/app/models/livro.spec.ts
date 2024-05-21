3f29298fb01ce8ba01f179dd4d757012e052cf41
Cássio Vagner dos Santos Diogo <cassiovsd@hotmail.com>
2024-05-21 20:55:35 -0300

    retirado livro

--- src/app/models/livro.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Livro } from './livro';
-
-describe('Livro', () => {
-  it('should create an instance', () => {
-    expect(new Livro()).toBeTruthy();
-  });
-});
--- src/app/models/livro.ts
+++
@@ -1,9 +0,0 @@
-export class Livro {
-    id!: number;
-    nome!: string;
-
-    constructor(id: number, nome: string){
-        this.id = id;
-        this.nome = nome;
-      }
-}
