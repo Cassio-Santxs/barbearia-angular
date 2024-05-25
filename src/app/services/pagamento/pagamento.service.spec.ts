c217964cb9d199abd3a7beef80c80debe1196a0a
Jean Carlos Liang Tan <jeanliangtan@hotmail.com>
2024-05-24 21:57:16 -0300

    Atualizacoes finais

--- src/app/services/pagamento/pagamento.service.spec.ts
+++ src/app/services/pagamento/pagamento.service.spec.ts
@@ -1,7 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
 import { PagamentoService } from './pagamento.service';
 
 describe('PagamentoService', () => {
-  it('should create an instance', () => {
-    expect(new PagamentoService()).toBeTruthy();
+  let service: PagamentoService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(PagamentoService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
   });
 });
